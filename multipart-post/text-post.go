c2ae6f5b2b37075431f4769312dbb82ef03fd334
tahira <tahira@pepabo.com>
2017-08-14 22:19:37 +0900

    add

---
+++ multipart-post/text-post.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("text hoge hoge")
+	resp, err := http.Post("http://localhost:18888", "text/plain", reader)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Status:", resp.Status)
+}
